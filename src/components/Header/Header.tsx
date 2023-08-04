3f238a57b660e9a431c778d9853512b87d775a76
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2023-08-04 12:52:57 -0400

    Header setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { memo } from "react";
+import MenuItem from "./MenuItem";
+// import { useWeb3React } from "@web3-react/core";
+// import { Flex, Box, FlexProps, Stack } from "@chakra-ui/react";
+// import { navHeightResponsive } from "uikit/theme/global";
+// import { ConnectWalletIconButton, ConnectWalletButton } from "components/Buttons/ConnectWalletButton";
+// import { LanguageSwitcherButtons } from "components/Buttons/LanguageSwitherButton/LanguageSwitcherButton";
+// import Logo from "./Logo";
+// import UserMenu from "./UserMenu";
+// import { MobileDrawer, LinkList } from "./NavLinks";
+// import { DesktopSearchButton, MobileSearchButton } from "./SearchButton";
+// import { useTranslation } from "next-i18next";
+// import { ColorModeButton } from "components/Buttons/ColorModeButton";
+
+// export type NavProps = FlexProps;
+
+const showOnMobileProps = { base: "block", md: "none" };
+const showOnDesktopProps = { base: "none", md: "block" };
 
 const Header = () => {
+  // const { account } = useWeb3React();
+  // const { t } = useTranslation();
+
   return (
-    <div>Header</div>
-  )
-}
+    <div 
+    className="flex items-center fixed top-0 left-0 w-full max-w-full sticky z-10 px-4 justify-between"
+    >
+      {/* <Logo mx={4} /> */}
+
+      <div className="flex items-center w-full hidden md:flex">
+        {/* <Box flexGrow={1} mx={4}>
+          <DesktopSearchButton />
+        </Box> */}
+        <MenuItem />
+      </div>
+
+      {/* <Stack direction="row" spacing={4}>
+        <Stack direction="row" spacing={0}>
+          <Box>
+            <ColorModeButton />
+          </Box>
+
+          <Box display={showOnDesktopProps}>
+            <LanguageSwitcherButtons variant="ghost" colorScheme="gray" />
+          </Box>
+        </Stack>
+        <Box display={showOnMobileProps}>
+          <MobileSearchButton />
+        </Box>
+        {account ? (
+          <UserMenu account={account} />
+        ) : (
+          <>
+            <Box display={{ base: "block", lg: "none" }}>
+              <ConnectWalletIconButton />
+            </Box>
+            <Box display={{ base: "none", lg: "block" }}>
+              <ConnectWalletButton round>{t("Connect")}</ConnectWalletButton>
+            </Box>
+          </>
+        )}
+        <Box display={showOnMobileProps} ml={3}>
+          <MobileDrawer />
+        </Box>
+      </Stack> */}
+    </div>
+  );
+};
 
 export default Header
--- src/components/Header/MenuItem/MenuItem.tsx
+++ src/components/Header/MenuItem/MenuItem.tsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import React from "react";
+import NextLink from "next/link";
+// import {
+//   Flex,
+//   FlexProps,
+//   IconButton,
+//   Drawer,
+//   DrawerBody,
+//   DrawerHeader,
+//   DrawerOverlay,
+//   DrawerContent,
+//   useDisclosure,
+//   Link,
+//   Box,
+//   Stack,
+//   Divider,
+//   useColorModeValue,
+//   MenuButton,
+//   MenuList,
+//   MenuItem,
+// } from "@chakra-ui/react";
+// import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+// import {
+//   // Badge,
+//   // ChevronDown,
+//   // ChevronUp,
+//   CloseIcon,
+//   CollectionIcon,
+//   CompassIcon,
+//   GiftIcon,
+//   HamburgerIcon,
+//   // RainbowText,
+//   // StaticLogoLarge,
+//   // StaticLogoLargeLight,
+// } from "uikit";
+// import { Menu } from "uikit/Menu/Menu";
+// import { LanguageSwitcherButtons } from "../Buttons/LanguageSwitherButton/LanguageSwitcherButton";
+// import { DesktopNextLink, MobileLink, MobileNextLink } from "./shared";
 
-const MenuItem = () => {
+const MenuItems = () => {
+  // const { t } = useTranslation();
+  const { pathname } = useRouter();
+  const isRewardActive = pathname.startsWith("/rewards");
   return (
-    <div>MenuItem</div>
-  )
-}
+    <div>
+      {/* <DesktopNextLink href="/explore">{t("Explore")}</DesktopNextLink>
+      <DesktopNextLink href="/collections">{t("Collections")}</DesktopNextLink> */}
+      <div >
+        <div
+          className="flex items-center px-4 h-12 border-b-2 whitespace-nowrap text-text-02 hover:bg-ui-01"  
+        >
+          {/* <RainbowText as="span" animate>
+            {t("Rewards")}
+          </RainbowText> */}
+        </div>
+        <div
+          // zIndex="dropdown"
+        >
+          <NextLink href="/rewards" passHref>
+            <div
+              // as="a"
+            >
+              {/* {t("Staking")} */}
+              staking
+            </div>
+          </NextLink>
+          <NextLink href="/rewards/trading" passHref>
+            <div
+              // as="a" display="flex" justifyContent="space-between"
+            >
+              {/* {t("Listing & Trading")} */}
+              Trading
+              {/* <Badge textTransform="uppercase" bg="support-error-inverse" color="white">
+                {t("New")}
+              </Badge> */}
+            </div>
+          </NextLink>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItems;
+
+// export const MobileDrawer = () => {
+//   const { t } = useTranslation();
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   const rewardDisclosure = useDisclosure();
+//   // const logoWithText = useColorModeValue(
+//   //   <StaticLogoLargeLight width="130" height="40" />,
+//   //   <StaticLogoLarge width="130" height="40" />
+//   // );
+
+//   return (
+//     <>
+//       <IconButton aria-label="Mobile menu" colorScheme="gray" isRound onClick={onOpen}>
+//         <HamburgerIcon />
+//       </IconButton>
+//       <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+//         <DrawerOverlay />
+//         <DrawerContent>
+//           <Flex alignItems="center" justifyContent="space-between" pr={4}>
+//             <DrawerHeader flex="0" display="flex" alignItems="end" tabIndex={1} px={4}>
+//               {/* <Box w="130px">{logoWithText}</Box> */}
+//             </DrawerHeader>
+//             <IconButton aria-label="close menu" variant="ghost" size="xs" color="text-01" onClick={onClose}>
+//               <CloseIcon />
+//             </IconButton>
+//           </Flex>
+//           <Box bg="ui-02">
+//             <LanguageSwitcherButtons layoutFixed />
+//           </Box>
+//           <DrawerBody display="flex" flexDirection="column" bgColor="ui-01" px={0}>
+//             <Stack spacing={0} divider={<Divider />}>
+//               {/* <MobileNextLink href="/explore" leftIcon={CompassIcon}>
+//                 {t("Explore")}
+//               </MobileNextLink> */}
+//               {/* <MobileNextLink href="/collections" leftIcon={CollectionIcon}>
+//                 {t("Collections")}
+//               </MobileNextLink>
+//               <MobileLink
+//                 as="div"
+//                 leftIcon={GiftIcon}
+//                 rightIcon={rewardDisclosure.isOpen ? <ChevronUp /> : <ChevronDown />}
+//                 onClick={rewardDisclosure.onToggle}
+//                 userSelect="none"
+//               >
+//                 {t("Rewards")}
+//               </MobileLink> */}
+//               {rewardDisclosure.isOpen && (
+//                 <>
+//                   {/* <MobileNextLink href="/rewards" pl="52px">
+//                     {t("Staking")}
+//                   </MobileNextLink>
+//                   <MobileNextLink
+//                     href="/rewards/trading"
+//                     pl="52px"
+//                     rightIcon={
+//                       <Badge textTransform="uppercase" bg="support-error-inverse" color="white">
+//                         {t("New")}
+//                       </Badge>
+//                     }
+//                   >
+//                     {t("Listing & Trading")}
+//                   </MobileNextLink> */}
+//                 </>
+//               )}
+//             </Stack>
+//           </DrawerBody>
+//         </DrawerContent>
+//       </Drawer>
+//     </>
+//   );
+// };
